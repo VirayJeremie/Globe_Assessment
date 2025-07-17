069cd0847e6fb17027c8e478761f09d38ed5f088
Jeremie Viray <jeremiejun.viray@gmail.com>
2025-07-18 01:52:13 +0800

    First Patch

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import re
 import asyncio
 from playwright.sync_api import sync_playwright,Page
 import pytest
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://demo.spreecommerce.org/')
+
+
+
+    page.wait_for_load_state()
+    browser.close
