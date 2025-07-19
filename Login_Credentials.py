7e9713f6d29d172216140725a1dd54e93de2a340
Jeremie Viray <jeremiejun.viray@gmail.com>
2025-07-19 14:09:33 +0800

    Seventh Patch

--- Login_Credentials.py
+++ Login_Credentials.py
@@ -3,6 +3,6 @@ import random
 class Credentials():
     def __init__(self):
         random_gen_num = random.randrange(0,1000000)
-        self.user_username = str(random_gen_num)+"@hotmail.com"
-        self.user_password = "manggo_123"
+        self.user_username = str(random_gen_num)+"@coldmail.com"
+        self.user_password = "rBDaH331<}A£"
         
